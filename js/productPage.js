802ddf486b295aae1dfea7be5c0199186363f23d
melsowole <82158719+melsowole@users.noreply.github.com>
2024-09-04 09:25:25 +0200

    moved script to productPage.js

---
+++ js/productPage.js
@@ -0,0 +1,51 @@
+import { products } from "./database/query.js";
+
+// Fetch product data by ID or index
+const productId = 0; // Example, this would be dynamic based on the page or URL
+const product = products.getById(productId);
+
+const mainImage = document.getElementById("main-image");
+const thumbnails = document.getElementById("thumbnail-row");
+const productTitle = document.getElementById("product-title");
+const productPrice = document.getElementById("product-price");
+const productDescription = document.getElementById("product-description");
+const showMoreText = document.getElementById("show-more-text");
+const sizeSelect = document.getElementById("size");
+
+// Update product details
+mainImage.src = product.images[0];
+product.images.forEach((image, index) => {
+  const thumbnail = document.createElement("img");
+  thumbnail.src = image;
+  thumbnail.alt = `Thumbnail ${index + 1}`;
+  thumbnail.classList.add("thumbnail");
+  thumbnail.addEventListener("click", () => {
+    mainImage.src = image;
+  });
+  thumbnails.appendChild(thumbnail);
+});
+
+productTitle.textContent = product.name;
+productPrice.textContent = `${product.priceSEK} SEK`;
+productDescription.textContent = product.description.split("<span")[0]; // show first part of description
+showMoreText.innerHTML = product.description.split("<span")[1] || ""; // show hidden part
+
+// Populate sizes
+product.sizes.forEach((size) => {
+  const option = document.createElement("option");
+  option.value = size;
+  option.textContent = size;
+  sizeSelect.appendChild(option);
+});
+
+// Show more funktion
+const showMoreLink = document.getElementById("show-more");
+showMoreLink.addEventListener("click", function () {
+  if (showMoreText.classList.contains("hidden")) {
+    showMoreText.classList.remove("hidden");
+    showMoreLink.textContent = "Show less";
+  } else {
+    showMoreText.classList.add("hidden");
+    showMoreLink.textContent = "Show more";
+  }
+});
