f933e54c7ab275a46402284f3bb06eb4d38265fa
Elvira Ericsson <elvira.rigi@hotmail.com>
2024-09-05 17:48:16 +0200

    updated basket

--- js/basket/continueButton.js
+++ js/basket/continueButton.js
@@ -1,11 +1,14 @@
 const infoContinueButton = document.getElementById('infoContinue');
+const shippingContinueButton = document.querySelector('.shippingContinue');
+
 const headerShipping = document.querySelector('.headerShipping');
 const shipping = document.getElementById('shipping');
+
 infoContinueButton.addEventListener('click', () => {
     const info = document.getElementById('info');
     const headerInfo = document.querySelector('.headerInfo');
 
-    shipping.style.display = 'block';
+    shipping.style.display = 'flex';
     info.style.display = 'none';
     headerShipping.style.backgroundColor = 'white';
     headerShipping.style.borderBottom = '0.5px solid black';
@@ -13,12 +16,11 @@ infoContinueButton.addEventListener('click', () => {
     headerInfo.style.border = 'none';
 })
 
-const shippingContinueButton = document.getElementById('shippingContinue');
 shippingContinueButton.addEventListener('click', () => {
     const headerPayment = document.querySelector('.headerPayment');
     const payment = document.getElementById('payment');
 
-    payment.style.display = 'block';
+    payment.style.display = 'flex';
     shipping.style.display = 'none';
     headerPayment.style.backgroundColor = 'white';
     headerPayment.style.borderBottom = '0.5px solid black';
