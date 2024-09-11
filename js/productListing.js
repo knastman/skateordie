f6eb3260b8539d07fe8ad7a60929580a1c49e521
Amandaanderssn <tindraamanda@hotmail.se>
2024-09-11 09:57:58 +0200

    påbörjad like knapp som ej fungerar helt

--- js/productListing.js
+++ js/productListing.js
@@ -18,35 +18,59 @@ function listAllProducts() {
             productCard.classList.add('productCard');
             productList.append(productCard);
 
+
             const productCardImg = document.createElement('img');
             productCardImg.src = product.images[0];
+
+            const wishlistButton = document.createElement("button");
+            wishlistButton.classList.add('wishlist-icon')
+            // wishlistButton.innerHTML = `<i class="fa-regular fa-heart fa-lg" class="wishlist-icon"></i>`
+            wishlistButton.innerHTML = `<i class="fa-solid fa-heart fa-lg" class="wishlist-icon"></i>`
+
 
 
             const productCardText = document.createElement('p');
             productCardText.classList.add('productCardText');
-            productCard.append(productCardImg, productCardText);
+            productCard.append(wishlistButton, productCardImg, productCardText);
+
 
 
             productCardText.innerHTML = `
             <p>${product.name}</p>
             <p>${product.priceSEK + " SEK"}</p>`;
 
-            productCard.addEventListener('mouseover', () => {
-                productCardImg.src = product.images[1];
+            productCard.addEventListener('mouseover', (event) => {
+                if (event.target == productCardImg) {
+                    productCardImg.src = product.images[1];
+                }
             })
             productCard.addEventListener('mouseout', () => {
                 productCardImg.src = product.images[0];
             })
 
+            // wishlistButton.addEventListener('mouseover', () => {
+            //     wishlistButton.innerHTML = `<i class="fa-solid fa-heart" class="wishlist-icon"></i>`
+            // })
+
+            // wishlistButton.addEventListener('mouseout', () => {
+            //     wishlistButton.innerHTML = `<i class="fa-regular fa-heart" class="wishlist-icon"></i>`
+            // })
+
             // CHOOSING A PRODUCT AND BEING SENT TO THE PRODUCTPAGE
 
-            productCard.addEventListener('click', () => {
-                console.log("product id is " + product.id)
-                console.log("The product category id is " + product.category_id)
+            productCard.addEventListener('click', (event) => {
+                if (event.target == productCardImg) {
+                    console.log("product id is " + product.id)
+                    console.log("The product category id is " + product.category_id)
 
-                localStorage.setItem('allProducts', product.id)
-                localStorage.setItem('allproducts', product.category_id)
-                window.location.href = `${window.location.origin}/produktsida.html`;
+                    localStorage.setItem('allProducts', product.id)
+                    localStorage.setItem('allproducts', product.category_id)
+                    window.location.href = `${window.location.origin}/produktsida.html`;
+                }
+                if (event.target == wishlistButton || wishlistButton.innerHTML) {
+                    console.log("test")
+                }
+
             })
         };
     });
