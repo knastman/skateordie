edeeb29dab954684bf77b077795e963337d5949d
Petra <144622676+knastman@users.noreply.github.com>
2024-09-12 17:15:36 +0200

    Wishlist - add to basket button functional

--- js/basket/init.js
+++ js/basket/init.js
@@ -12,7 +12,7 @@ document.addEventListener("click", (e) => {
   // BUYBUTTON FUNCTIONALITY
   if (e.target.classList.contains("add-to-cart")) {
     //identify item
-    const item = e.target.closest(".productCard") || e.target.closest(".product-page");
+    const item = e.target.closest(".productCard") || e.target.closest(".product-page") || e.target.closest(".wishlist-add-basket");
 
     // add item to basket
     basket.add(item.id);
